fcc2221d2df6bd66b0f7500d8a4d9c63241370a4
Carlo Vespa <vespac@usi.ch>
2014-11-21 00:46:17 +0100

    [Close #4] Player keyboard controls
    Player: Add input handlers for movement.
    Game: Load Input module and setup keyboard key codes.

--- src/StreamerClient/js/game.js
+++ src/StreamerClient/js/game.js
@@ -14,12 +14,24 @@
  */
 window.onload = function() {
     var Q = window.Q = Quintus({development: true}) // remove development option to enable asset caching
-        .include('Sprites')
+        .include('Sprites, Input')
         .setup({
             width: 1280,
             height: 720,
             scaleToFit: true
         });
+
+    // bind keycodes to Quintus events
+    Q.input.keyboardControls({
+        LEFT: "leftKey",
+        RIGHT: "rightKey",
+        UP: "upKey",
+        DOWN: "downKey",
+        81: "qKey",
+        87: "wKey",
+        69: "eKey",
+        82: "rKey"
+    });
 
     PlayerClass(Q);
 
--- src/StreamerClient/js/player.js
+++ src/StreamerClient/js/player.js
@@ -21,9 +21,104 @@ function PlayerClass(Q) {
          * @param p properties object, can be null
          */
         init: function (p) {
+            // call default constructor
             this._super(p, {
-                asset: PLAYER_SPRITE_FILE
+                asset: PLAYER_SPRITE_FILE,
+                direction: 'right',
+                vx: 0,
+                vy: 0,
+                speed: 200,
+                isJumping: false
             });
+
+            // bind action buttons to the respective handlers
+            Q.input.on('qKey', this, this.onActionQ);
+            Q.input.on('wKey', this, this.onActionW);
+            Q.input.on('eKey', this, this.onActionE);
+            Q.input.on('rKey', this, this.onActionR);
+
+            Q.input.on('upKey', this, this.onActionJump);
+
+            // bind event handlers
+            this.on('landed', this, this.onLand);
+        },
+
+        step: function(dt) {
+            // update horizontal speed and sprite according to input
+            if (Q.inputs['leftKey'] && Q.inputs['rightKey']) {
+                if (this.p.direction == 'left') {
+                    this.p.flip = false;
+                }
+                this.p.direction = 'right';
+                this.p.vx = 0;
+            } else if (Q.inputs['leftKey']) {
+                if (this.p.direction == 'right') {
+                    this.p.flip = 'x';
+                }
+                this.p.direction = 'left';
+                this.p.vx = this.p.isJumping ? -this.p.speed / 2 : -this.p.speed;
+            } else if (Q.inputs['rightKey']) {
+                if (this.p.direction == 'left') {
+                    this.p.flip = false;
+                }
+                this.p.direction = 'right';
+                this.p.vx = this.p.isJumping ? this.p.speed / 2 : this.p.speed;
+            } else {
+                this.p.vx = 0;
+            }
+
+            // fake gravity
+            this.p.vy += 800 * dt;
+
+            // update position
+            this.p.x += this.p.vx * dt;
+            this.p.y += this.p.vy * dt;
+
+
+            // testing code, will be substituted by stages and collisions
+            // sets an invisible floor at 200px from the bottom
+            // sets invisible walls at the sides
+            if (this.p.y + this.p.cy > Q.height - 200) {
+                this.p.y = Q.height - 200 - this.p.cy;
+                this.p.vy = 0;
+
+                this.trigger('landed');
+            }
+
+            if (this.p.x + this.p.cx > Q.width) {
+                this.p.x = Q.width - this.p.cx;
+            }
+
+            if (this.p.x - this.p.cx < 0) {
+                this.p.x = this.p.cx;
+            }
+        },
+
+        onActionQ: function() {
+            alert('Pressed Q');
+        },
+
+        onActionW: function() {
+            alert('Pressed W');
+        },
+
+        onActionE: function() {
+            alert('Pressed E');
+        },
+
+        onActionR: function() {
+            alert('Pressed R');
+        },
+
+        onActionJump: function() {
+            if (!this.p.isJumping) {
+                this.p.isJumping = true;
+                this.p.vy = -500;
+            }
+        },
+
+        onLand: function() {
+            this.p.isJumping = false;
         }
     });
 }
